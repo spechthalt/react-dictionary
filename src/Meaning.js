965aa8f2152f10703c2b6e8095ac08a50c79c5e0
Stephanie <stephpechthalt@aol.com>
2022-02-24 09:42:35 -0800

    added phonetics but need to work out some empty link bugs

--- src/Meaning.js
+++ src/Meaning.js
@@ -3,7 +3,6 @@ import Example from "./Example";
 import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
-  console.log(props.meaning);
   return (
     <div>
       <h3>{props.meaning.partOfSpeech}</h3>
