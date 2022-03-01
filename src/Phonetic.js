4ec92689c0755877475929c4d2cf4246492ef542
Stephanie <stephpechthalt@aol.com>
2022-02-28 20:44:37 -0800

    fixed warning for a

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -6,7 +6,7 @@ export default function Phonetic(props) {
   if (props.phonetic.audio) {
     return (
       <div className="Phonetic">
-        <a href={props.phonetic.audio} target="_blank">
+        <a href={props.phonetic.audio} target="_blank" rel="noreferrer>
           Listen
         </a>{" "}
         {props.phonetic.text}
